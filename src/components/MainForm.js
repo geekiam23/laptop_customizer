3b0cfc391a891c60a7ac0920ec90b5d86687b53c
Will Larry <will23larry@gmail.com>
2019-04-03 00:07:27 -0400

    Refactor App.js into smaller components

---
+++ src/components/MainForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Feature from "./Feature";
+
+class MainForm extends React.Component {
+  render() {
+    const features = Object.keys(this.props.features).map(key => (
+      <Feature
+        key={key}
+        name={key}
+        features={this.props.features[key]}
+        handleUpdate={this.props.handleUpdate}
+        selected={this.props.selected}
+      />
+    ));
+
+    return (
+      <section className="main__form">
+        <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+        {features}
+      </section>
+    );
+  }
+}
+
+export default MainForm;
